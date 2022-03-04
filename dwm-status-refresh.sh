d687aaa46bc3d8f21abc8b2543c38a85bf19f795
UnlimitedWand <UnlimitedWand@nobody.com>
2022-03-04 21:17:03 +0800

    fix speed moniter problem

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -4,44 +4,52 @@
 
 # This function parses /proc/net/dev file searching for a line containing $interface data.
 # Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
-function get_bytes {
-	# Find active network interface
-	interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
-	line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
-	eval $line
-	now=$(date +%s%N)
-}
 
-# Function which calculates the speed using actual and old byte number.
-# Speed is shown in KByte per second when greater or equal than 1 KByte per second.
-# This function should be called each second.
-
-function get_velocity {
-	value=$1
-	old_value=$2
-	now=$3
-
-	timediff=$(($now - $old_time))
-	velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
-	if test "$velKB" -gt 1024
-	then
-		echo $(echo "scale=2; $velKB/1024" | bc)MB/s
-	else
-		echo ${velKB}KB/s
-	fi
-}
-
-# Get initial values
-get_bytes
-old_received_bytes=$received_bytes
-old_transmitted_bytes=$transmitted_bytes
-old_time=$now
+# Global variables
+#interface=""
+#received_bytes=""
+#old_received_bytes=""
+#transmitted_bytes=""
+#old_transmitted_bytes=""
+#
+#function get_bytes {
+#	# Find active network interface
+#	interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
+#	line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
+#	eval $line
+#	now=$(date +%s%N)
+#}
+#
+## Function which calculates the speed using actual and old byte number.
+## Speed is shown in KByte per second when greater or equal than 1 KByte per second.
+## This function should be called each second.
+#
+#function get_velocity {
+#	value=$1
+#	old_value=$2
+#	now=$3
+#
+#	timediff=$(($now - $old_time))
+#	velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
+#	if test "$velKB" -gt 1024
+#	then
+#		echo $(echo "scale=2; $velKB/1024" | bc)MB/s
+#	else
+#		echo ${velKB}KB/s
+#	fi
+#}
+#
+## Get initial values
+#get_bytes
+#old_received_bytes=$received_bytes
+#old_transmitted_bytes=$transmitted_bytes
+#old_time=$now
 
 print_volume() {
 	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
 	if test "$volume" -gt 0
 	then
-		echo -e "\uE05D${volume}"
+		echo -e "${volume}"
 	else
 		echo -e "Mute"
 	fi
@@ -76,30 +84,47 @@ get_time_until_charged() {
 	echo $pretty_time;
 }
 
-get_battery_combined_percent() {
-
-	# get charge of all batteries, combine them
-	total_charge=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc));
-
-	# get amount of batteries in the device
-	battery_number=$(acpi -b | wc -l);
-
-	percent=$(expr $total_charge / $battery_number);
-
-	echo $percent;
-}
-
 get_battery_charging_status() {
 
-	if $(acpi -b | grep --quiet Discharging)
+	if $(acpi -b | grep -i --quiet " charging")
 	then
-		echo "🔋";
+		echo ":";
 	else # acpi can give Unknown or Charging if charging, https://unix.stackexchange.com/questions/203741/lenovo-t440s-battery-status-unknown-but-charging
-		echo "🔌";
+		echo ":";
 	fi
 }
 
+get_battery_combined_percent() {
 
+	# get charge of all batteries, combine them
+	if $(acpi -b | grep --quiet "Not charging")
+	then
+		total_charge="";
+	else
+		total_charge=$(acpi -b | awk '{print $4}');
+	fi
+	# get amount of batteries in the device
+	#battery_number=$(acpi -b | wc -l);
+
+	#percent=$(expr $total_charge / $battery_number);
+
+	echo $total_charge;
+}
+
+get_battery_remain_time() {
+
+	# get charge of all batteries, combine them
+	charging_status=$(get_battery_charging_status);
+	
+	#if [ "$charging_status" == "nc" ]; then
+	remain_time=$(acpi -b | awk '{print $5}');
+	# get amount of batteries in the device
+	#battery_number=$(acpi -b | wc -l);
+
+	#percent=$(expr $total_charge / $battery_number);
+
+	echo $remain_time;
+}
 
 print_bat(){
 	#hash acpi || return 0
@@ -116,11 +141,11 @@ print_bat(){
 		#echo -e "${charge}"
 	#fi
 	#echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
-	echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
+	echo "$(get_battery_charging_status) $(get_battery_combined_percent) $(get_battery_remain_time)";
 }
 
 print_date(){
-	date '+%Y年%m月%d日 %H:%M'
+	date '+%Y.%m.%d. %H:%M'
 }
 
 show_record(){
@@ -150,17 +175,18 @@ export IDENTIFIER="unicode"
 #. "$DIR/dwmbar-functions/dwm_ccurse.sh"
 #. "$DIR/dwmbar-functions/dwm_date.sh"
 
-get_bytes
+#get_bytes
+#
+## Calculates speeds
+#vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
+#vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-# Calculates speeds
-vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
-vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
-
-xsetroot -name "  💿 $(print_mem)M ⬇️ $vel_recv ⬆️ $vel_trans $(dwm_alsa) [ $(print_bat) ]$(show_record) $(print_date) "
+xsetroot -name "   $(print_mem)M ﰬ$1 ﰵ$2 $(dwm_alsa) [ $(print_bat) ]  $(print_date) "
+echo "$1 $2"
 
 # Update old values to perform new calculations
-old_received_bytes=$received_bytes
-old_transmitted_bytes=$transmitted_bytes
-old_time=$now
+#old_received_bytes=$received_bytes
+#old_transmitted_bytes=$transmitted_bytes
+#old_time=$now
 
 exit 0
--- dwmbar-functions/dwm_alsa.sh
+++ dwmbar-functions/dwm_alsa.sh
@@ -8,20 +8,23 @@
 
 dwm_alsa () {
     VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+    NOTMUTE=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)\].*/\1/")
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$VOL" -eq 0 ]; then
-            printf "🔇"
+    	if [ "$NOTMUTE" = "off" ]; then
+            printf "婢 %s%%" "$VOL"
+        elif [ "$VOL" -eq 0 ]; then
+            printf "奄 %s%%" "$VOL"
         elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "🔈 %s%%" "$VOL"
+            printf "奔 %s%%" "$VOL"
         elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "🔉 %s%%" "$VOL"
+            printf "墳 %s%%" "$VOL"
         else
-            printf "🔊 %s%%" "$VOL"
+            printf " %s%%" "$VOL"
         fi
     else
-        if [ "$VOL" -eq 0 ]; then
-            printf "MUTE"
+    	if [ "$NOTMUTE" = "off" ]; then
+            printf "MUTE %s%%" "$VOL"
         elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
             printf "VOL %s%%" "$VOL"
         elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
